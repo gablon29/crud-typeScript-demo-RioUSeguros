d7290ca8686b3546c4486baa763e72932c858669
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-12 09:16:44 -0300

    initial estructured finished

---
+++ src/components/ListaUsers.tsx
@@ -0,0 +1,114 @@
+import {
+	Card,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeaderCell,
+	TableRow,
+} from "@tremor/react";
+
+const usersData: {
+	id: string;
+	name: string;
+	email: string;
+	age: string;
+	gitHub: string;
+}[] = [
+	{
+		id: "1",
+		name: "Gabriel Londero",
+		email: "gabriellondero40@gmail.com",
+		age: "28",
+		gitHub: "gablon29",
+	},
+	{
+		id: "2",
+		name: "Lucas Londero",
+		email: "lucaslondero@gmail.com",
+		age: "31",
+		gitHub: "lucasDev",
+	},
+	{
+		id: "3",
+		name: "Florencia Pacheco",
+		email: "ritaflorenciap8@gmail.com",
+		age: "28",
+		gitHub: "florenciaFront",
+	},
+];
+
+export default function ListUsers() {
+	return (
+		<Card>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableHeaderCell> Id </TableHeaderCell>
+						<TableHeaderCell> Name </TableHeaderCell>
+						<TableHeaderCell> Email </TableHeaderCell>
+						<TableHeaderCell> Acciones </TableHeaderCell>
+					</TableRow>
+				</TableHead>
+
+				<TableBody>
+					{usersData.map((item) => (
+						<TableRow key={item.id}>
+							<TableCell style={{ display: "flex", alignItems: "center" }}>
+								<img
+									style={{
+										width: "32px",
+										height: "32px",
+										borderRadius: "50%",
+										marginRight: "8px",
+									}}
+									src={`https://unavatar.io/github/${item.gitHub}`}
+									alt={item.name}
+								/>
+								{item.name}
+							</TableCell>
+							<TableCell>{item.email}</TableCell>
+							<TableCell>
+								<button type="button">
+									<svg
+										style={{ marginRight: "5px" }}
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="w-6 h-6"
+									>
+										<title>Editar</title>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+										/>
+									</svg>
+								</button>
+								<button type="button">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="w-6 h-6"
+									>
+										<title>Eliminar</title>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+										/>
+									</svg>
+								</button>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</Card>
+	);
+}
